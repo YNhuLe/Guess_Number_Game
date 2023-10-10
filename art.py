0b589cbe29072c68d33896246b4f3282e80d5c7c
NhuYLe <lenhuy@sheridancollege.ca>
2023-10-10 00:26:18 -0400

    First commit

---
+++ art.py
@@ -0,0 +1,25 @@
+logo ="""                                                                                                                                                                                                                             
+                                                                                                                                                                   bbbbbbbb                                                    
+        GGGGGGGGGGGGG                                                                             NNNNNNNN        NNNNNNNN                                         b::::::b                                                    
+     GGG::::::::::::G                                                                             N:::::::N       N::::::N                                         b::::::b                                                    
+   GG:::::::::::::::G                                                                             N::::::::N      N::::::N                                         b::::::b                                                    
+  G:::::GGGGGGGG::::G                                                                             N:::::::::N     N::::::N                                          b:::::b                                                    
+ G:::::G       GGGGGGuuuuuu    uuuuuu      eeeeeeeeeeee        ssssssssss       ssssssssss        N::::::::::N    N::::::Nuuuuuu    uuuuuu     mmmmmmm    mmmmmmm   b:::::bbbbbbbbb        eeeeeeeeeeee    rrrrr   rrrrrrrrr   
+G:::::G              u::::u    u::::u    ee::::::::::::ee    ss::::::::::s    ss::::::::::s       N:::::::::::N   N::::::Nu::::u    u::::u   mm:::::::m  m:::::::mm b::::::::::::::bb    ee::::::::::::ee  r::::rrr:::::::::r  
+G:::::G              u::::u    u::::u   e::::::eeeee:::::eess:::::::::::::s ss:::::::::::::s      N:::::::N::::N  N::::::Nu::::u    u::::u  m::::::::::mm::::::::::mb::::::::::::::::b  e::::::eeeee:::::eer:::::::::::::::::r 
+G:::::G    GGGGGGGGGGu::::u    u::::u  e::::::e     e:::::es::::::ssss:::::ss::::::ssss:::::s     N::::::N N::::N N::::::Nu::::u    u::::u  m::::::::::::::::::::::mb:::::bbbbb:::::::be::::::e     e:::::err::::::rrrrr::::::r
+G:::::G    G::::::::Gu::::u    u::::u  e:::::::eeeee::::::e s:::::s  ssssss  s:::::s  ssssss      N::::::N  N::::N:::::::Nu::::u    u::::u  m:::::mmm::::::mmm:::::mb:::::b    b::::::be:::::::eeeee::::::e r:::::r     r:::::r
+G:::::G    GGGGG::::Gu::::u    u::::u  e:::::::::::::::::e    s::::::s         s::::::s           N::::::N   N:::::::::::Nu::::u    u::::u  m::::m   m::::m   m::::mb:::::b     b:::::be:::::::::::::::::e  r:::::r     rrrrrrr
+G:::::G        G::::Gu::::u    u::::u  e::::::eeeeeeeeeee        s::::::s         s::::::s        N::::::N    N::::::::::Nu::::u    u::::u  m::::m   m::::m   m::::mb:::::b     b:::::be::::::eeeeeeeeeee   r:::::r            
+ G:::::G       G::::Gu:::::uuuu:::::u  e:::::::e           ssssss   s:::::s ssssss   s:::::s      N::::::N     N:::::::::Nu:::::uuuu:::::u  m::::m   m::::m   m::::mb:::::b     b:::::be:::::::e            r:::::r            
+  G:::::GGGGGGGG::::Gu:::::::::::::::uue::::::::e          s:::::ssss::::::ss:::::ssss::::::s     N::::::N      N::::::::Nu:::::::::::::::uum::::m   m::::m   m::::mb:::::bbbbbb::::::be::::::::e           r:::::r            
+   GG:::::::::::::::G u:::::::::::::::u e::::::::eeeeeeee  s::::::::::::::s s::::::::::::::s      N::::::N       N:::::::N u:::::::::::::::um::::m   m::::m   m::::mb::::::::::::::::b  e::::::::eeeeeeee   r:::::r            
+     GGG::::::GGG:::G  uu::::::::uu:::u  ee:::::::::::::e   s:::::::::::ss   s:::::::::::ss       N::::::N        N::::::N  uu::::::::uu:::um::::m   m::::m   m::::mb:::::::::::::::b    ee:::::::::::::e   r:::::r            
+        GGGGGG   GGGG    uuuuuuuu  uuuu    eeeeeeeeeeeeee    sssssssssss      sssssssssss         NNNNNNNN         NNNNNNN    uuuuuuuu  uuuummmmmm   mmmmmm   mmmmmmbbbbbbbbbbbbbbbb       eeeeeeeeeeeeee   rrrrrrr            
+                                                                                                                                                                                                                               
+     """
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,74 @@
+from random import randint
+from art import logo
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+PRO_LEVEL = 2
+win_win = 0
+
+
+# TODO: Function to check user's guess against actual answer.
+def check_answer(guess, answer, turns):
+    """Checks answer against guess. Returns the number of turns remaining."""
+    if guess > answer:
+        print("Too high.")
+        return turns - 1
+    elif guess < answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"Yay!!!You got it! The answer was {answer}.")
+
+
+# TODO: Make function to set difficulty
+def set_difficulty():
+    level = input("We have 3 levels for you to pick from.\n"
+                  "1. Easy peasy, when you have 10 guesses.\n"
+                  "2. Something that can be 'A real struggle', when you only have 5 guesses.\n"
+                  "3. 'I am a GURU', when you only have 2 guesses.\n"
+                  "Please pick your level: ")
+    if level == "1":
+        return EASY_LEVEL_TURNS
+    elif level == "2":
+        return HARD_LEVEL_TURNS
+    elif level == "3":
+        return PRO_LEVEL
+    else:
+        print("You need to pick from 3 levels above.")
+
+
+def game():
+    print(logo)
+    # TODO: Choosing a random number between 1 and 100
+
+    print("I am thinking the number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"The correct answer is {answer}")
+
+    turns = set_difficulty()
+
+    # TODO: Repeat the functionalities if they get it wrong
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        # Let guess  a number:
+        guess = int(input("Make a guess: "))
+        # Track the number of turns and reduce by 1 if they get it wrong
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run put of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again!")
+
+
+while True:
+    play = input("Ready for the new challenge with the Number Guessing Game! Please type 'yes' to play and 'no' to "
+                 "exit the game. ").lower()
+    if play == "yes":
+        game()
+    elif play == "no":
+        print("Let's level up yourself next time!!!!")
+        break
+    else:
+        print("Invalid input.")
